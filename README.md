134ccbe408b7fa1ef9afe5848b7a3257ff52f69e
Simon Wyckaert <simon@digitold.com>
2018-02-13 22:52:10 +0100

    tests model admin

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Sur Heroku:
 * Ancien: https://j27-gossip-swy.herokuapp.com/ (MAUVAIS REPO, plus valide)
 * Nouveau: https://j27-realgossips-swy.herokuapp.com/ (BON REPO, galère pour le recréer celui-là)
 
+L'app est fonctionnel en local est sur Heroku.
+
+Avec tous ces problèmes de repos sur Github et Heroku, mes tests ont un comportement bizarres sur mon poste. Il faudrait que je reparte de zéro. Donc je ne sais pas trop ce que ça donnera pour vous :)
+
 ---
 
 Slack: @simwyck
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation tests' do
+    it 'username is not empty' do
+      admin = Admin.new(email: "me@domain.com").save
+      expect(admin).to eq(false)
+    end
+
+    it 'username is not spaces only' do
+      admin = Admin.new(username: "    ", email: "me@domain.com").save
+      expect(admin).to eq(false)
+    end
+
+    it 'email is not empty' do
+      admin = Admin.new(username: "Username").save
+      expect(admin).to eq(false)
+    end
+
+    it 'email is correctly formated' do
+      admin = Admin.new(username: "First", email: "me.domain.com").save
+      admin = Admin.new(username: "First", email: "me@domaincom").save
+      expect(admin).to eq(false)
+    end
+
+    it 'admin is correctly saved' do
+      admin = Admin.new(username: "First", email: "me@domain.com").save
+      expect(admin).to eq(true)
+    end
+
+  end
 end
